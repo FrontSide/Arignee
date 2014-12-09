a7bb6fec20134c638f0f89dc8b8f79d77ed2ea6a
drieger <drieger@gmx.at>
2014-12-09 10:57:07 +0000

    start google trends keyword evaluation

---
+++ public/javascripts/trends_kw.js
@@ -0,0 +1,9 @@
+/* Project Arignee ** autor: David Rieger ** 2014 */
+
+/* Trends KeyWord Fetcher */
+
+/* This JS sends an HTTP request to google trends and 
+ * requests json data for the given keyphrase entered by the user 
+ */
+ 
+ 
